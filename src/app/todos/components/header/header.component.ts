071ebfb776221965b76a569745360999b5d9c971
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-07-26 12:27:34 +0300

    Header development

--- src/app/todos/components/header/header.component.ts
+++ src/app/todos/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos-header',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  
+  text: string = '';
 
+  constructor( private toDoService: TodoService) {
+
+  }
+
+  changeText(event:Event): void {
+    const target = event.target as HTMLInputElement;
+
+    this.text = target.value;
+  }
+
+  addTodo() {
+    this.toDoService.addTodo(this.text);
+    this.text= '';
+  }
 }
