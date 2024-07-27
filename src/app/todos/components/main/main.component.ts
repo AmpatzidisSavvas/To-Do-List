4deaa958634fc430832442b14f5e5ae52a5a012c
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-07-27 09:39:08 +0300

    Implementing Main section

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todos-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
 })
 export class MainComponent {
-
+  
+  constructor(public todoService: TodoService) {}
 }
