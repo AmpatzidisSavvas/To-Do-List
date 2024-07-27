ba037500f70e31f217cb94ef926999a056337d80
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-07-27 10:07:22 +0300

    Filter todos

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { CommonModule } from '@angular/common';
+import { FilterEnum } from '../../types/filter.enum';
 
 @Component({
   selector: 'app-todos-main',
@@ -10,5 +11,20 @@ import { CommonModule } from '@angular/common';
 })
 export class MainComponent {
   
+
   constructor(public todoService: TodoService) {}
+
+
+  visibleTodos = computed(() => {
+
+    const todos = this.todoService.todoSig();
+    const filter = this.todoService.fiterSig();
+
+    if (filter === FilterEnum.active) {
+      return todos.filter((todo) => !todo.isCompleted);
+    } else if (filter === FilterEnum.completed) {
+      return todos.filter((todo) => todo.isCompleted);
+    }
+    return todos;
+  })
 }
